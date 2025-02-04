962f66f19b680ef036e0704060ceab4555112555
Parv Mehta <mehtaparv21@gmail.com>
2025-02-04 23:13:46 +0530

    File Handling in Nextjs with Imagekit

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export async function GET() {
+  try {
+    const authenticationParameter = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authenticationParameter);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Image kit failed",
+      },
+      { status: 500 }
+    );
+  }
+}
